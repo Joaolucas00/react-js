d4840fbc56feefc3f23af3bf2a85f9a2d179b1fd
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-01-22 11:42:14 -0300

    componente

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing
+function Message() {
+    // JSX: JavaScript XML
+    const nome = "João";
+    if (nome)
+        return <h1>Hello {nome}</h1>;
+    return <h1>Hello World</h1>
+}
+
+export default Message;
