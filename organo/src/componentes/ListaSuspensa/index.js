295c86f4708f45ba11277b751c51c51b4a174a9b
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-02-07 13:39:17 -0300

    .

--- organo/src/componentes/ListaSuspensa/index.js
+++ organo/src/componentes/ListaSuspensa/index.js
@@ -1,7 +1,7 @@
 import './ListaSuspensa.css'
 const ListaSuspensa = (props) => {
     return (
-        <div className="campo-texto">
+        <div className="campo-texto lista-suspensa">
             <label>{props.label}</label>
             <select>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
