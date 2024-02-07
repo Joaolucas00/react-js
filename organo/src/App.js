f25a229bb2530f9c9f0f5306626a8586dc656d83
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-02-07 20:51:25 -0300

    componente time

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/time';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoCadastrar={cadastro => aoCadastro(cadastro)} titulo="Formulário" method="get" action="/" autocomplete="on"/>
+      <Time titulo="PHP"/>
     </div>
   );
 }
---
+++ organo/src/componentes/time/index.js
@@ -0,0 +1,10 @@
+import './Time.css'
+
+const Time = (props) => {
+    return (
+        <section className='time'>
+            <h3>{props.titulo}</h3>
+        </section>)
+}
+
+export default Time
