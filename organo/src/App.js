6b1f9e6e5360055ddb8593a3d5e0320a402902e7
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-02-08 10:28:17 -0300

    times

--- organo/src/App.js
+++ organo/src/App.js
@@ -5,6 +5,44 @@ import Time from './componentes/time';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'DevOps',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    }
+  ]
+
   const [cadastrado, setCadastrado] = useState([])
 
   const aoCadastro = (colaborador) => {
@@ -16,6 +54,9 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoCadastrar={cadastro => aoCadastro(cadastro)} titulo="Formulário" method="get" action="/" autocomplete="on"/>
+
+    {times.map(time => <Time titulo={time.nome} />)}
+
       <Time titulo="PHP"/>
     </div>
   );
