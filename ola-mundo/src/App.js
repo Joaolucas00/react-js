c6df0caa2369f1384d0d1263021030a1c5a8e20a
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-02-11 10:59:35 -0300

    react router dom

--- ola-mundo/src/App.js
+++ ola-mundo/src/App.js
@@ -1,9 +1,20 @@
-
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
+  //const pagina = window.location.pathname === "/sobremim" ? <SobreMim/> : <Inicio/> JavaScript puro
+
+
   return (
-      <h1>Olá, Mundo!</h1>
-  );
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Inicio/>} />
+          <Route path="/sobremim" element={<SobreMim/>}/>
+          <Route path="*" element={<h1>Página não encontrada</h1>}/>
+        </Routes>
+      </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ ola-mundo/src/paginas/SobreMim/index.js
@@ -0,0 +1,11 @@
+
+
+const SobreMim = () => {
+    return (
+        <h1>Sobre Mim</h1>
+    )
+}
+
+export default SobreMim
+
+
