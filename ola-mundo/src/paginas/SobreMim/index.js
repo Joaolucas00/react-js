10691e40498af7edc27979d4c6592e3cf3076e78
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-02-13 11:51:26 -0300

    sobre mim

--- ola-mundo/src/paginas/SobreMim/index.js
+++ ola-mundo/src/paginas/SobreMim/index.js
@@ -1,8 +1,14 @@
-
+import PostModelo from "componentes/PostModelo"
+import fotoCapa from 'assets/sobre_mim_capa.png'
+import styles from './SobreMim.module.css'
+import fotoSobreMim from 'assets/erro_404.png'
 
 const SobreMim = () => {
     return (
-        <h1>Sobre Mim</h1>
+        <PostModelo fotoCapa={fotoCapa} titulo="Sobre Mim">
+            <h3 className={styles.subtitulo}> Olá, eu sou</h3>
+            <img src={fotoSobreMim} alt="foto minha" className={styles.fotoSobreMim}/>
+        </PostModelo>
     )
 }
 
