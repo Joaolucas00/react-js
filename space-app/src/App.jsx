2392129f155fc79b5c0e3a7be9872aa5f126a8c0
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-02-20 10:24:20 -0300

    .

--- space-app/src/App.jsx
+++ space-app/src/App.jsx
@@ -3,20 +3,37 @@ import EstilosGlobais from './componentes/EstilosGlobais'
 import Cabecalho from './componentes/Cabecalho'
 import BarraLateral from './componentes/BarraLateral'
 import Banner from './componentes/Banner'
+import Galeria from './componentes/Galeria'
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
   min-height: 100vh;
   `
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
+`
 
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  align-items: center;
+`
 
 function App() {
   return (
     <FundoGradiente>
+      <AppContainer>
+        <Cabecalho/>
+        <BarraLateral/>
+        <ConteudoGaleria>
+          <Galeria/>
+        </ConteudoGaleria>
+      </AppContainer>
       <EstilosGlobais/>
-      <Cabecalho/>
-      <BarraLateral/>
     </FundoGradiente>
   )
 }
---
+++ space-app/src/componentes/Galeria/index.jsx
@@ -0,0 +1,11 @@
+
+
+
+
+const Galeria = () => {
+    return (
+        <h2 style={{margin: "1% 50% 1% 1%"}}>Navegue pela galeria</h2>
+    )
+}
+
+export default Galeria
