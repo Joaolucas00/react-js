8f3e7ea3f6f20b400011691931044aa0db051a99
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-03-03 16:25:29 -0300

    uuid

--- alura-studies/src/componentes/ListaDeEstudos/index.tsx
+++ alura-studies/src/componentes/ListaDeEstudos/index.tsx
@@ -2,19 +2,28 @@ import { useState } from "react"
 import { IListaDeEstudos } from "../../interfaces/IListaDeEstudos"
 import style from './style.module.scss'
 import Lista from "./Lista"
+import {v4 as uuIdv4} from 'uuid'
+
 
 const ListaDeEstudos = () => {
 
     const [listaDeEstudos, setListaDeEstudos] = useState<IListaDeEstudos[]>([
-        {aula: "Java", tempo: "01:00:00"},
+        {id: uuIdv4(), aula: "Java", tempo: "01:00:00"},
+        {id: uuIdv4(), aula: "Java", tempo: "01:00:00"},
+        {id: uuIdv4(), aula: "Java", tempo: "01:00:00"},
+        {id: uuIdv4(), aula: "Java", tempo: "01:00:00"},
+        {id: uuIdv4(), aula: "Java", tempo: "01:00:00"},
+        {id: uuIdv4(), aula: "Java", tempo: "01:00:00"},
+        {id: uuIdv4(), aula: "Java", tempo: "01:00:00"},
+        {id: uuIdv4(), aula: "Java", tempo: "01:00:00"},
     ]) 
 
     return (
         (listaDeEstudos.length > 0) ? <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
-                {listaDeEstudos.map((estudo, index) => {
-                    return (<Lista key={index} listaDeEstudos={estudo}/>)
+                {listaDeEstudos.map((estudo) => {
+                    return (<Lista key={estudo.id} listaDeEstudos={estudo}/>)
                 })} 
             </ul> 
         </aside> : <aside className={style.listaTarefas}><h2>Não há estudos</h2></aside>
--- alura-studies/src/interfaces/IListaDeEstudos.ts
+++ alura-studies/src/interfaces/IListaDeEstudos.ts
@@ -1,4 +1,5 @@
 export interface IListaDeEstudos {
+    id: string
     aula: string;
     tempo: string
 }
