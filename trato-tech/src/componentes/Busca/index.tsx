80f2ae7352e1841b3fb68b06ad131f64c72e9e1d
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-03-26 15:17:40 -0300

    navbar

---
+++ trato-tech/src/componentes/Busca/index.tsx
@@ -0,0 +1,14 @@
+import styles from './Busca.module.scss'
+
+const Busca = () => {
+    return (
+        <div className={styles.busca}>
+            <input
+                className={styles.input}
+                placeholder='O que você busca?'
+            />
+        </div>
+    )
+}
+
+export default Busca
--- trato-tech/src/componentes/NavBar/index.tsx
+++ trato-tech/src/componentes/NavBar/index.tsx
@@ -1,12 +1,29 @@
 import styles from './NavBar.module.scss'
 import logo from '../../assets/logo.svg'
+import { Link, useLocation } from 'react-router-dom'
+import { RiShoppingCart2Line } from 'react-icons/ri'
+import Busca from '../Busca'
+
 
 const NavBar = () => {
+
+    const localizacao = useLocation()
+    
     return (
         <nav className={styles.nav}>
             <img className={styles.logo} src={logo} alt='Logo trato tech'></img>
             <div className={styles.link}>
-                <h1>Ola</h1>
+                <div>
+                <Link to='/' className={`${ localizacao.pathname === '/' ? styles.selected : styles.link}`}>Página inicial</Link>
+                </div>
+            </div>
+            <div className={styles.busca}>
+                <Busca/>
+            </div>
+            <div className={styles.icones}>
+                <Link to='/carrinho'>
+                    <RiShoppingCart2Line color='white' size={24}/>
+                </Link>
             </div>
         </nav>
     )
