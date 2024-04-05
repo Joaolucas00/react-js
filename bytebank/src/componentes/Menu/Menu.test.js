3831bd2ebe4b130b83be777c14c4c5b0fa3dae50
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-04-05 17:35:40 -0300

    testing library

--- bytebank/src/componentes/Menu/Menu.test.js
+++ bytebank/src/componentes/Menu/Menu.test.js
@@ -5,16 +5,23 @@ test('Deve redentrizar um link para a página inicial', () => {
     render(<Menu/>)
     const linkParaPaginaInicial = screen.getByText('Inicial')
     expect(linkParaPaginaInicial).toBeInTheDocument()
-})
+});
 
 test('Deve redentrizar uma lista de links', () => {
     render(<Menu/>)
     const listaDeLinks = screen.getAllByRole('link')
     expect(listaDeLinks).toHaveLength(4)
-})
+});
 
 test('Não deve redenrizar para Extrato', () => {
     render(<Menu/>)
     const linkExtrato = screen.queryByText('Extrato')
     expect(linkExtrato).not.toBeInTheDocument()
+});
+
+test('Deve redenrizar uma lista de links com a classe link', () => {
+    render(<Menu/>);
+    const links = screen.getAllByRole('link');
+    links.forEach((link) => expect(link).toHaveClass('link'));
+    expect(links).toMatchSnapshot()
 })
