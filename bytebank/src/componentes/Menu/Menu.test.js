adaa6a2af970178140037923ecf3e890eea8014d
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-04-05 17:50:51 -0300

    Update Menu.test.js

--- bytebank/src/componentes/Menu/Menu.test.js
+++ bytebank/src/componentes/Menu/Menu.test.js
@@ -1,27 +1,27 @@
-import { screen, render } from '@testing-library/react'
-import Menu from '.'
+import { screen, render } from '@testing-library/react';
+import Menu from '.';
 
 test('Deve redentrizar um link para a página inicial', () => {
-    render(<Menu/>)
-    const linkParaPaginaInicial = screen.getByText('Inicial')
-    expect(linkParaPaginaInicial).toBeInTheDocument()
+    render(<Menu/>);
+    const linkParaPaginaInicial = screen.getByText('Inicial');
+    expect(linkParaPaginaInicial).toBeInTheDocument();
 });
 
 test('Deve redentrizar uma lista de links', () => {
-    render(<Menu/>)
-    const listaDeLinks = screen.getAllByRole('link')
-    expect(listaDeLinks).toHaveLength(4)
+    render(<Menu/>);
+    const listaDeLinks = screen.getAllByRole('link');
+    expect(listaDeLinks).toHaveLength(4);
 });
 
 test('Não deve redenrizar para Extrato', () => {
-    render(<Menu/>)
-    const linkExtrato = screen.queryByText('Extrato')
-    expect(linkExtrato).not.toBeInTheDocument()
+    render(<Menu/>);
+    const linkExtrato = screen.queryByText('Extrato');
+    expect(linkExtrato).not.toBeInTheDocument();
 });
 
 test('Deve redenrizar uma lista de links com a classe link', () => {
     render(<Menu/>);
     const links = screen.getAllByRole('link');
     links.forEach((link) => expect(link).toHaveClass('link'));
-    expect(links).toMatchSnapshot()
-})
+    expect(links).toMatchSnapshot();
+});
