2c3bbb2970b8580312d95f54c4efb4e2a91635cc
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-04-06 19:12:35 -0300

    Update Formulario.test.js

--- bytebank/src/componentes/Transacao/Formulario/Formulario.test.js
+++ bytebank/src/componentes/Transacao/Formulario/Formulario.test.js
@@ -24,4 +24,21 @@ describe('Deve redenrizar um campo de input', () => {
     })
 })    
 
+test('Deve chamar um evento de onSubmit ao clicar em realizar transação', () => {
+    const realizarTransacao = jest.fn()
+    render(<Formulario realizarTransacao={realizarTransacao}/>)
+    const botao = screen.getByRole('button')
 
+    userEvent.click(botao)
+    expect(realizarTransacao).toHaveBeenCalledTimes(1)
+})
+
+test('Deve ser possível selecionar uma opção do elemento select', () => {
+    render(<Formulario/>)
+    const select = screen.getByRole('combobox')
+    const optionTransacao = screen.getByRole('option', {name: 'Selecione um tipo de transação'})
+    const optionDeposito = screen.getByRole('option', {name: 'Depósito'})
+    userEvent.selectOptions(select, ['Depósito'])
+    expect(optionTransacao.selected).toBe(false)
+    expect(optionDeposito.selected).toBe(true)
+})
