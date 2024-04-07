21581fbacc2165391d333c8f99f70d0a9e53dfbc
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-04-07 19:23:08 -0300

    Create integracoes.test.js

---
+++ bytebank-v2/src/services/integracoes.test.js
@@ -0,0 +1,62 @@
+import api from './api'
+import { buscaTransacoes, salvaTransacao } from './transacoes'
+
+jest.mock('./api')
+
+const mockTransacao = [{
+    id: 1,
+    transacao: 'Depósito',
+    valor: '100',
+    data: '22/11/2022',
+    mes: 'Novembro'
+}]
+
+const mockRequisicao = (retorno) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                data: retorno
+            })
+        }, 200)
+    })
+}
+
+const mockRequisicaoErro = () => {
+    return new Promise((_, reject) => {
+        setTimeout(() => { reject() }, 200)
+    })
+}
+
+const mockRequisicaoPost = () => {
+    return new Promise((resolve) => { resolve({ status: 201}) }, 200)
+}
+
+beforeEach(() => {
+    api.get.mockClear();
+})
+
+describe('Requisições para API', () => {
+    test('Deve retornar uma lista de transações', async () => {
+        api.get.mockImplementation(() => mockRequisicao(mockTransacao))
+        const transacoes = await buscaTransacoes();
+        expect(transacoes).toEqual(mockTransacao);
+        expect(api.get).toHaveBeenCalledWith('/transacoes');
+    })
+
+    test('Deve retornar uma lista vazia quando a requisição falhar', async () => {
+        api.get.mockImplementation(() => mockRequisicaoErro());
+
+        const transacoes = await buscaTransacoes();
+
+        expect(transacoes).toEqual([]);
+        expect(api.get).toHaveBeenCalledWith('/transacoes');
+    })
+
+    test('Deve retornar um status 201 após uma requisição POST', async () => {
+        api.post.mockImplementation(() => mockRequisicaoPost())
+        const status = await salvaTransacao(mockTransacao[0])
+        expect(status).toBe(201)
+        expect(api.post).toHaveBeenCalledWith('/transacoes', mockTransacao[0])
+    })
+    
+})
