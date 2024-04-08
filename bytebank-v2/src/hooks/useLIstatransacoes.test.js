d31b247df2512f4a80b7d0d3be2116a43f57d7ab
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-04-08 12:32:58 -0300

    hooks test

---
+++ bytebank-v2/src/hooks/useLIstatransacoes.test.js
@@ -0,0 +1,26 @@
+import { renderHook, act } from "@testing-library/react";
+import { buscaTransacoes } from '../services/transacoes'
+import useListaTransacoes from './useListaTransacoes'
+
+jest.mock('../services/transacoes')
+
+const mockTransacao = [{
+    id: 1,
+    transacao: 'Depósito',
+    valor: '100',
+    data: '22/11/2022',
+    mes: 'Novembro'
+}]
+
+describe('hooks/useListaTransacoes.js', () => {
+    test('Deve retornar uma lista de transações e uma fução que atualiza', async () => {
+        buscaTransacoes.mockImplementation(() => mockTransacao)
+        const { result } = renderHook(() => useListaTransacoes())
+
+        await act(async () => {
+            result.current[1]()
+        })
+
+        expect(result.current[0]).toBe(mockTransacao)
+    })
+})
