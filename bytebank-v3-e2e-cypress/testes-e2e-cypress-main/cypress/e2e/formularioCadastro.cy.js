56608737554f67a4839caca2546356ae4bef4a08
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-04-10 15:44:50 -0300

    _

---
+++ bytebank-v3-e2e-cypress/testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
@@ -0,0 +1,18 @@
+describe('Formulário cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://localhost:3000')
+    })
+
+    /*
+    it('Usuário deve conseguir se cadastrar com sucesso', () => {
+        cy.getByData('botao-cadastro').click()
+        cy.getByData('nome-input').type('Jão lucas')
+        cy.getByData('email-input').type('Jaolucas@emailqualquer.com')
+        cy.getByData('senha-input').type('Jãolucas1234')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-sucesso').should('exist').and('have.text', 'Usuário cadastrado com sucesso')
+    })
+    */
+   
+})
