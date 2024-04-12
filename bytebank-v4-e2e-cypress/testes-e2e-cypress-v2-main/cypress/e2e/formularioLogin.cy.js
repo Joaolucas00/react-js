ee705b65ea812aed4b7734453ab3623ad8812690
Joaolucas00 <122113495+Joaolucas00@users.noreply.github.com>
2024-04-12 15:10:44 -0300

    _

--- bytebank-v4-e2e-cypress/testes-e2e-cypress-v2-main/cypress/e2e/formularioLogin.cy.js
+++ bytebank-v4-e2e-cypress/testes-e2e-cypress-v2-main/cypress/e2e/formularioLogin.cy.js
@@ -1,22 +1,20 @@
 describe('Formulario de Login', ()=>{
-  beforeEach(()=>{
-    cy.visit('http://localhost:3000')
-  })
 
-  it('Deve acessar a página home', () => {
-    cy.fazerLogin()
+  it.only('Deve acessar a página home', () => {
+    cy.logar('aaa@aaa.com', '12345')
+    cy.visit('/home')
     cy.getByData('titulo-boas-vindas').should('contain', 'Bem vindo de volta!')
-  })
+  });
 
   it('Não deve permitir um email inválido', ()=>{
     cy.logar('emailerrado@errado', 'senhaqualquer')
     cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
-  })
+  });
 
   it('Não deve permitir um campo em branco', ()=>{
     cy.getByData('botao-login').click()
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
     cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório')
-  })
-})
+  });
+});
--- bytebank-v4-e2e-cypress/testes-e2e-cypress-v2-main/cypress/support/commands.js
+++ bytebank-v4-e2e-cypress/testes-e2e-cypress-v2-main/cypress/support/commands.js
@@ -10,8 +10,12 @@ Cypress.Commands.add('fazerLogin', () => {
 })
 
 Cypress.Commands.add('logar', (email, senha) => {
-  cy.getByData('botao-login').click()
-  cy.getByData('email-input').type(email)
-  cy.getByData('senha-input').type(senha)
-  cy.getByData('botao-enviar').click()
+  cy.session([email, senha], () => {
+      cy.visit('/')
+      cy.getByData('botao-login').click()
+      cy.getByData('email-input').type(email)
+      cy.getByData('senha-input').type(senha)
+      cy.getByData('botao-enviar').click()
+      cy.url().should('contain', '/home')
+  })
 })
